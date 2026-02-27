2cbe919a38aca08475227f57e94188a11cb1c670
Mustafa Akdaş <makdas163@gmail.com>
2026-02-28 00:53:39 +0300

    Catelog API created and model  folder added

---
+++ src/Services/Catalog/Catalog.API/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace Catalog.API.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public decimal Price { get; set; }
+        public List<string> Category{ get; set; } = new ();
+        public string ImageFile { get; set; } = null!;
+    }
+}
