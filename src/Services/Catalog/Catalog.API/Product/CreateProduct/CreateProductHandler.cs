f16f24eafc69376542f74a5662674eb0b090fc65
Mustafa Akdaş <makdas163@gmail.com>
2026-03-07 01:11:57 +0300

    Cqrs implemented

--- src/Services/Catalog/Catalog.API/Product/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Product/CreateProduct/CreateProductHandler.cs
@@ -1,14 +1,26 @@
-﻿using MediatR;
-
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
 namespace Catalog.API.Product.CreateProduct
 {
-    public record CreateProductCommand(string Name, string Description, decimal Price, List<string> Category, string ImageFile) : IRequest<CreateProductCommandResponse>;
-    public record CreateProductCommandResponse(Guid Id);
-    internal class CreateProductHandler : IRequestHandler<CreateProductCommand, CreateProductCommandResponse>
+    public record CreateProductCommand(string Name, string Description, decimal Price, List<string> Category, string ImageFile)
+        : ICommand<CreateProductCommandResult>;
+    public record CreateProductCommandResult(Guid Id);
+    internal class CreateProductHandler : ICommandHandler <CreateProductCommand, CreateProductCommandResult>
     {
-        public Task<CreateProductCommandResponse> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        public async Task<CreateProductCommandResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var entity = new Products
+            {
+                Name = request.Name,
+                Description = request.Description,
+                Price = request.Price,
+                Category = request.Category,
+                ImageFile = request.ImageFile
+
+            };
+
+            return new CreateProductCommandResult(Guid.NewGuid());
+
         }
     }
 }
