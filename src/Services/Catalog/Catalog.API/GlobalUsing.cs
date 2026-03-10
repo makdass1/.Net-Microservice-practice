023bc2d728021fe7467487f6994a9f5fb3726dcf
Mustafa Akdaş <makdas163@gmail.com>
2026-03-10 13:53:40 +0300

    NugetPackages installed at project and designed to Createproduct folder

---
+++ src/Services/Catalog/Catalog.API/GlobalUsing.cs
@@ -0,0 +1,5 @@
+﻿﻿global using Carter;
+global using MediatR;
+global using BuildingBlocks.CQRS;
+global using FluentValidation;
+global using Mapster;
--- src/Services/Catalog/Catalog.API/Product/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Product/CreateProduct/CreateProductHandler.cs
@@ -1,15 +1,26 @@
 ﻿using BuildingBlocks.CQRS;
-using Catalog.API.Models;
 namespace Catalog.API.Product.CreateProduct
 {
     public record CreateProductCommand(string Name, string Description, decimal Price, List<string> Category, string ImageFile)
         : ICommand<CreateProductCommandResult>;
     public record CreateProductCommandResult(Guid Id);
+
+    public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
+    {
+        public CreateProductCommandValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required");
+            RuleFor(x => x.Category).NotEmpty().WithMessage("Category is required");
+            RuleFor(x => x.ImageFile).NotEmpty().WithMessage("ImageFile is required");
+            RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price must be greater than 0");
+        }
+    }
+
     internal class CreateProductHandler : ICommandHandler <CreateProductCommand, CreateProductCommandResult>
     {
         public async Task<CreateProductCommandResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
         {
-            var entity = new Products
+            var entity = new Models.Products
             {
                 Name = request.Name,
                 Description = request.Description,
